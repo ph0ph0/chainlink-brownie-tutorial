fe293b6c9516e58e0cb402d8f84d3120c88063be
Phill <20524533+ph0ph0@users.noreply.github.com>
2022-07-28 21:58:09 +0100

    fixed bug in fund_advanced_collectible

--- nft-from-scratch/scripts/helpful_scripts.py
+++ nft-from-scratch/scripts/helpful_scripts.py
@@ -1,6 +1,22 @@
 from brownie import AdvancedCollectible, accounts, config, interface, network
 
-def fund_advanced_collectible(nft_contract):    
-    dev = accounts.add([config['wallets']["from_key"]])
-    link_token = interface.LinkTokenInterface(config['networks'][network.show_active()]['link_token'])
-    link_token.transfer(nft_contract, 100000000000000000, {'from': dev})
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+
+
+def fund_advanced_collectible(nft_contract):
+    dev = get_account()
+    link_token = interface.LinkTokenInterface(
+        config["networks"][network.show_active()]["link_token"]
+    )
+    link_token.transfer(nft_contract, 100000000000000000, {"from": dev})
+
+
+def get_account():
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
