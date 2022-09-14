9ba22b6188b3951996998743997cfb42eae7985d
Phill <20524533+ph0ph0@users.noreply.github.com>
2022-09-14 08:52:23 +0100

    finish

---
+++ nft-from-scratch/metadata/rinkeby/urls/dog_ipfs_urls.py
@@ -0,0 +1,5 @@
+dog_urls = {
+    "PUG": "ipfs://Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json",
+    "SHIBA_INU": "ipfs://QmdryoExpgEQQQgJPoruwGJyZmz6SqV4FRTX1i73CT3iXn?filename=1-SHIBA_INU.json",
+    "ST_BERNARD": "ipfs://QmbBnUjyHHN7Ytq9xDsYF9sucZdDJLRkWz7vnZfrjMXMxs?filename=2-ST_BERNARD.json",
+}
--- nft-from-scratch/scripts/advanced_collectible/set_tokenuri.py
+++ nft-from-scratch/scripts/advanced_collectible/set_tokenuri.py
@@ -1,5 +1,8 @@
-from brownie import AdvancedCollectible, network
-from scripts.helpful_scripts import get_breed
+from brownie import AdvancedCollectible, network, interface
+from scripts.helpful_scripts import get_breed, get_account
+from metadata.rinkeby.urls import dog_ipfs_urls
+
+OPENSEA_FORMAT = OPENSEA_FORMAT = "https://testnets.opensea.io/assets/{}/{}"
 
 
 def main():
@@ -10,10 +13,28 @@ def main():
         "Number of advanced collectibles deployed is "
         + str(number_of_advanced_collectibles)
     )
+
+    urls = dog_ipfs_urls.dog_urls
+
     for token_id in range(number_of_advanced_collectibles):
-        breed = getBreed(advanced_collectible.tokenIdToBreed(token_id))
+        breed = get_breed(advanced_collectible.tokenIdToBreed(token_id))
+        print("url for {}: ".format(breed) + str(urls[breed]))
         if advanced_collectible.tokenURI(token_id).startswith("None"):
             print("Setting tokenURI of {}".format(token_id))
-            set_tokenURI(token_id, advanced_collectible, ???)
-            
-    
+            set_tokenURI(token_id, advanced_collectible, urls[breed])
+        else:
+            print("Skipping {}, we've already set that tokenURI".format(token_id))
+
+
+def set_tokenURI(token_id, nft_contract, tokenURI):
+    dev = get_account()
+    transaction = nft_contract.setTokenURI(token_id, tokenURI, {"from": dev})
+    # Wait for confirmations
+    transaction.wait(1)
+    print("Transaction: " + str(transaction))
+    print(
+        "Awesome! You can now view your nft on OpenSea at ".format(
+            OPENSEA_FORMAT.format(nft_contract.address, token_id)
+        )
+    )
+    print("Please give up to 20 mins and hit the refresh metadata button")
